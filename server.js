09f921e609ac3645c6e9579343a00ad3fd1a1928
QiuziWu <wuqiuzi0829@163.com>
2025-08-12 18:54:41 -0700

    Add Netlify domain to CORS configuration for production deployment

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const PORT = 4000;
 
 // 中间件
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:5174', 
+    'http://localhost:3000',
+    'https://qiuziwu-a6-kambaz-react-web-app.netlify.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
